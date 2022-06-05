7b4837149f1af497eb31649b13ad371b0fbc0eb3
AgniTanmay <agnitanmay99132@gmail.com>
2022-06-05 11:06:50 +0530

    assignment-get-api

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,19 +1,27 @@
-const printDate = function() {
-    let currentDate = new Date()
-    console.log(currentDate)    
-}
+const res = require("express/lib/response");
 
-const printMonth = function() {
-    let currentdate = new Date()
-    let currentMonth = currentdate.getMonth() + 1
-    console.log('The current month is '+currentMonth)
+const movieList=(req,res)=>{
+  const movieName=["A perfect Pairing","Senior Year","Jackass 4.5","Dangerous","Sonic the Hedgedog","Top Gun","Cleveland Abduction","Turbo"];
+ res.send(movieName);
 }
+module.exports.movieList=movieList;
 
-const getBatchInfo = function() {
-  let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
-  console.log(batchInformation)
+const moviesOfList=(req,res,indexNumber)=>{
+  const movieName=["A perfect Pairing","Senior Year","Jackass 4.5","Dangerous","Sonic the Hedgedog","Top Gun","Cleveland Abduction","Turbo"];
+  var i =req.params.indexNumber;
+  var j =i-1;
+  if(i<=movieName.length){
+    res.send(movieName[j])
+  }else{
+    res.send("Invalid List")
+  }
 }
+module.exports.moviesOfList=moviesOfList;
 
-module.exports.printDate = printDate
-module.exports.getCurrentMonth = printMonth
-module.exports.getCohortData = getBatchInfo
+const movieComp =(req,res)=>{
+  const movieComp = [{Name:"A perfect pairing",id:1},{Name:"Senior Year",id:2},{Name:"Jackass 4.5",id:3},{Name:"Dangerous",id:4},{Name:"Sonic the Hedgehog",id:5}];
+  res.send(movieComp);
+}
+module.exports.movieComp=movieComp;
+
+
