b6132a59a08efc8f6b961625338a1ade5dec236c
AgniTanmay <agnitanmay99132@gmail.com>
2022-06-07 08:31:33 +0530

    find-list-of-books

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://WASIF321:Ansari738871@wasifdatabase.wdcjr.mongodb.net/Agni-Radon", {
+mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
