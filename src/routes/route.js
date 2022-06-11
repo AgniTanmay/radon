d88d45094d722646e347241ca2ead6f1f1eb6b04
AgniTanmay <agnitanmay99132@gmail.com>
2022-06-11 10:54:21 +0530

    initial

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
+router.get("/test-me", function (req, res){
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
+router.post("/createAuthor", authorController.createAuthor)
 
 router.get("/getAuthorsData", authorController.getAuthorsData)
 
-router.post("/createBook", bookController.createBook  )
+router.post("/createBook", bookController.createBook)
 
 router.get("/getBooksData", bookController.getBooksData)
 
